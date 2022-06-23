2ff03680c7e244c5ea996edfd63d341c4c3d6b4f
Ara Kanic <ji.arakani@gmail.com>
2022-06-23 13:09:54 -0400

    Update reverselongwords.js

--- reverselongwords.js
+++ reverselongwords.js
@@ -1,6 +1,6 @@
 // The following function takes in a string of one or more words, and returns the same string, but with words longer than seven characters reversed.
 
-function spinWords(string){
+function sevenWords(string) {
     return string.split(" ")
       .map(word => {
         return word.length > 7 ? word.split("").reverse().join("") : word
